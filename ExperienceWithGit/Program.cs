78798baed1a24ef92f54ce5de7ddcbdd2c993eb5
seristom <sergej.istomin@publicispixelpark.de>
2018-04-24 15:38:29 +0200

    feature/primzahlen_add isPrime() function

--- ExperienceWithGit/Program.cs
+++ ExperienceWithGit/Program.cs
@@ -11,8 +11,31 @@ namespace ExperienceWithGit
             
             Console.WriteLine("Serist,");
             Console.WriteLine(",...");
+            int max = Int32.MaxValue;
+            for (int i = 0; i < max; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i + "ist eine Primzahl");
+                }
+            }
+            Console.Read();
+        }
 
-            Console.ReadLine();
+        public static bool IsPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
         }
     }
 }

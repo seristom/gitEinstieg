5a60350671b94e60b91daa19ce80fafdc94c2cb4
seristom <sergej.istomin@publicispixelpark.de>
2018-04-24 14:39:56 +0200

    init commit

---
+++ ExperienceWithGit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExperienceWithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Today: 12.12.1995");
+			Console.WriteLine("Developer in this Project are: ");
+            
+            //TODO: Write your name here and push all your changes to Github.
+            Console.WriteLine("John Doe,");
+            Console.WriteLine("Jane Doe,");
+            Console.WriteLine(",...");
+
+            Console.ReadLine();
+        }
+    }
+}
